27e82d1c0933335091dbd437213320fe0361619d
Niel Geysen <niel.geysen@student.ucll.be>
2026-06-07 15:26:38 +0200

    Add react integrations for the untiteldUI icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import icon from "astro-icon";
 
 import node from "@astrojs/node";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
-  integrations: [icon()],
+  integrations: [icon(), react()],
 });
