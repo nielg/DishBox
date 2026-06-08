472789ad04200ee7ea80b6f917f485d37337f81f
Niel Geysen <niel.geysen@student.ucll.be>
2026-06-08 20:11:22 +0200

    Clean up package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
